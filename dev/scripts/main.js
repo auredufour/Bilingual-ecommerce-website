638d8fe762d7dfd094196da26b597c6d42d6c407
Aurelie Dufour <aure.dufour@gmail.com>
2016-04-03 17:25:41 -0400

    ecommerce website

---
+++ dev/scripts/main.js
@@ -0,0 +1,10 @@
+const ohHey = "Hello World";
+
+console.log(ohHey);
+
+$("#search").on('click', function(){
+	$(".left form").toggle("slide");
+	$(".left i").toggle();
+})
+
+
---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+console.log(ohHey);
+
+$("#search").on('click', function () {
+	$(".left form").toggle("slide");
+	$(".left i").toggle();
+});
